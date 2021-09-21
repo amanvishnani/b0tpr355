82d9c64db41f4ab3d8540c18a70f93dd95170da6
Aman Vishnani <aman.vishnani@dal.ca>
2021-09-21 16:15:41 -0300

    Fiddeling with file system API

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log('Hello World')
+const fs = require('fs').promises;
+const path = require('path');
+const [node, fileName, ...directories] = process.argv;
+
+console.log(`${directories.length}`)
+
+// Root
+const fileSystem = {}
+
+async function main() {
+    for (const dir of directories) {
+        try {
+            const filePath = path.resolve(dir);
+            const dirName = path.basename(filePath);
+            fileSystem[dirName] = await fs.readdir(dir, { withFileTypes: true });
+        } catch (error) {
+            if(error.code === 'ENOENT') {
+                console.log(`Invalid folder path: ${dir}. Skipping!`)
+            }
+        }
+    }
+    console.log(fileSystem);
+}
+
+main();
